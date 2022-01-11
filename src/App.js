f814cd1e036ef21b7770cc118c2260916bbf2d4a
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-01-11 09:39:13 -0300

    Total refactoring of project -- start by changing the App add the switch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import Routes from './routes/Routes';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
-function App() {
-  return <Routes />;
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/carteira" render={ () => <Wallet /> } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;

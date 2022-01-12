5f148f6615a5743c71e1cb5d28ca5e7fe29e70dd
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-01-12 11:31:59 -0300

    changes in the Table component -- not working, expenses not reached in state

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+    console.log(expenses);
     return (
       <div>
         {/* the ideia of table ---> https://www.w3schools.com/tags/tag_table.asp */}
         <table>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir </th>
-          </tr>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir </th>
+            </tr>
+          </thead>
+          {/* <tbody>
+            <tr>Teste</tr>
+            {expenses.map((expense) => (
+              <td>{expense.</td>
+            ))}
+          </tbody> */}
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(Table);

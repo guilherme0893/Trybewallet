56261d6656e0aef168b08ba49073d61eba1d652b
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-01-17 16:58:23 -0300

    teste

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+// alteração para commit teste
 
 class Header extends Component {
   constructor(props) {

96f01ee12a8eb94cdfb4ed2764beb7d42c89f288
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-17 00:33:59 -0300

    chore: Adds the new MainHeader and adds style

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,22 +2,23 @@ import React from 'react';
 import ExpensesForm from '../components/ExpensesForm';
 import Header from '../components/Header';
 import Table from '../components/Table';
+import MainHeader from '../components/MainHeader';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <div>
-        <header>
-          <h1
-            className="border-b-4 text-5xl text-center m-auto max-w-lg font-bold"
-          >
-            Trybewallet
-          </h1>
-        </header>
+      <main
+        className="h-100 flex-col flex items-center justify-center pb-80"
+        style={ {
+          backgroundColor: '#9921e8',
+          backgroundImage: 'linear-gradient(315deg, #cc99cc, #999999 75%)',
+        } }
+      >
+        <MainHeader />
         <Header />
         <ExpensesForm />
         <Table />
-      </div>
+      </main>
     );
   }
 }

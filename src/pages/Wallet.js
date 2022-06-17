47e7eaf848025d3eed68236b9cf86fbbc0d58aab
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-17 15:04:06 -0300

    refact: Removes MainHeader component & style: remove unused style

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,19 +2,11 @@ import React from 'react';
 import ExpensesForm from '../components/ExpensesForm';
 import Header from '../components/Header';
 import Table from '../components/Table';
-import MainHeader from '../components/MainHeader';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <main
-        className="h-100 flex-col flex items-center justify-center pb-80"
-        style={ {
-          backgroundColor: '#9921e8',
-          backgroundImage: 'linear-gradient(315deg, #cc99cc, #999999 75%)',
-        } }
-      >
-        <MainHeader />
+      <main>
         <Header />
         <ExpensesForm />
         <Table />
